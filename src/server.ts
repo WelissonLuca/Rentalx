6c634c98fc692b5edaf7b1bfa9fc229430a18840
Welisson luca <welissonluca17@gmail.com>
2021-03-16 23:29:09 -0300

    feat: environment script dev update

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-import express from 'express';
-
-const app  = express();
+import express, { request, response } from 'express';
+const app = express();
 
 app.listen(3333, () => console.log('Server is running'));
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello world'})
+});
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name })
+})
