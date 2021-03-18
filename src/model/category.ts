a47a0bd9ee91c0a83f1fc2124c18bbbca76044b2
Welisson luca <welissonluca17@gmail.com>
2021-03-18 00:03:33 -0300

    feat: category model add

---
+++ src/model/category.ts
@@ -0,0 +1,8 @@
+class Category {
+  id: string;
+  name: string;
+  description: string;
+  created_at: Date;git 
+}
+
+export {  Category }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
 import { v4 as uuidV4 } from 'uuid'
+import { Category } from '../model/category';
 const categoriesRoutes = Router();
 
-const categories = []
+const categories: Category[] = []
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
+  const category: Category = {
     name,
     description,
     id: uuidV4(),
+    created_at: new Date()
   };
   categories.push(category);
 
