c626ea35c298628b817e85152717f03223133aac
Welisson luca <welissonluca17@gmail.com>
2021-03-18 00:27:54 -0300

    feat: repositories add, Category repositorie configured complete add

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,19 @@
 import { Router } from 'express';
 import { v4 as uuidV4 } from 'uuid'
-import { Category } from '../model/category';
+import { CategoriesRepositorie } from '../repositories/CategoriesRepositories'
+
+
 const categoriesRoutes = Router();
 
-const categories: Category[] = []
+const categoriesRepositorie = new CategoriesRepositorie
+
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  });
+  categoriesRepositorie.create({ name, description });
 
-
-  categories.push(category);
-
-  return response.status(201).json({category});
+  return response.status(201).send();
 
 })
 
