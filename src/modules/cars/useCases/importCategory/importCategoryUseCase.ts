5b941777728e21454c5036392acf132d809f9e68
Welisson luca <welissonluca17@gmail.com>
2021-03-28 20:01:00 -0300

    feat: insert data categories import

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -30,8 +30,16 @@ class ImportCategoryUseCase {
    })
   }
  async execute(file: Express.Multer.File): Promise<void> {
-    const categories = await this.loadCategories(file);
-    console.log(categories);
+   const categories = await this.loadCategories(file);
+   categories.map(async category => {
+     const { name, description } = category;
+
+     const existCategory = await this.categoriesRepository.findByName(name);
+
+     if (!existCategory)
+       this.categoriesRepository.create({name, description})
+
+   })
   }
 }
 
