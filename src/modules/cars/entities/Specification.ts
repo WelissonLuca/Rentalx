37e2d0a10b66ee9ded1d724c8d26429e9711a6d4
Welisson luca <welissonluca17@gmail.com>
2021-05-28 00:20:43 -0300

    feat: entity specification add

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,9 +1,15 @@
 import { v4 as uuidV4 } from 'uuid';
+import { Column, Entity, PrimaryColumn, CreateDateColumn } from 'typeorm';
 
+@Entity('specifications')
 class Specification {
+  @PrimaryColumn()
   id?: string;
+  @Column()
   name: string;
+  @Column()
   description: string;
+  @CreateDateColumn()
   created_At: Date;
 
   constructor() {
