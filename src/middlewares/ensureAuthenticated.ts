3db9cbe376bef9cd106db754d25fdcefc76875f9
Welisson luca <welissonluca17@gmail.com>
2021-06-01 00:41:13 -0300

    feat: add authenticated permission alter avatar profile

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -26,6 +26,10 @@ export async function ensureAuthenticated(
     const user = usersRepository.findById(user_id);
 
     if (!user) throw new AppError('User does not exists', 401);
+
+    request.user = {
+      id: user_id,
+    };
     next();
   } catch (error) {
     throw new AppError('Invalid token!', 401);
