aa304493ab07a9ce9b7ba59c5af5e1260bbb73fe
Welisson luca <welissonluca17@gmail.com>
2021-08-26 14:48:59 -0300

    feat: add  initial specs car

---
+++ src/modules/cars/useCases/createCars/CreateCarUseCase.spec.ts
@@ -0,0 +1,23 @@
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarRepositoryInMemory';
+import { CreateCarUseCase } from './CreateCarUseCase';
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+describe('Create Car', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+  it('should be able to create a new car', async () => {
+    await createCarUseCase.execute({
+      name: 'name car',
+      description: 'Description car',
+      daily_rate: 100,
+      available: true,
+      license_plate: 'BCA-123',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'categori',
+    });
+  });
+});
