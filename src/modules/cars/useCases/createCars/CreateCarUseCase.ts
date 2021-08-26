49c09010e35913e5c555e14399c5f51ada0c88d7
Welisson luca <welissonluca17@gmail.com>
2021-08-26 14:48:50 -0300

    feat: add initial use case car

---
+++ src/modules/cars/useCases/createCars/CreateCarUseCase.ts
@@ -0,0 +1,43 @@
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { inject, injectable } from 'tsyringe';
+
+interface IRequest {
+  name: string;
+  description: string;
+  daily_rate: number;
+  available: boolean;
+  license_plate: string;
+  fine_amount: number;
+  brand: string;
+  category_id: string;
+}
+@injectable()
+class CreateCarUseCase {
+  constructor(
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository
+  ) {}
+  async execute({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    available,
+    fine_amount,
+    brand,
+    category_id,
+  }: IRequest): Promise<void> {
+    this.carsRepository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      available,
+      fine_amount,
+      brand,
+      category_id,
+    });
+  }
+}
+
+export { CreateCarUseCase };
