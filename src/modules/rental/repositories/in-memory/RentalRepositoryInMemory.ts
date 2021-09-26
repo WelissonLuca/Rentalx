edf1bb38b9272f2ba7bffc5fa24570d5b22377f6
Welisson luca <welissonluca17@gmail.com>
2021-09-25 21:24:44 -0300

    feat: add rental repository

---
+++ src/modules/rental/repositories/in-memory/RentalRepositoryInMemory.ts
@@ -0,0 +1,44 @@
+import { ICreateRentalDTO } from '@modules/rental/dtos/ICreateRentalDTO';
+import { Rental } from '@modules/rental/infra/typeorm/entities/Rental';
+import { IRentalsRepository } from '../IRentalsRepository';
+
+class RentalRepositoryInMemory implements IRentalsRepository {
+  private rentals: Rental[] = [];
+
+  async create({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = new Rental();
+
+    Object.assign(rental, {
+      user_id,
+      car_id,
+      expected_return_date,
+      start_date: new Date(),
+    });
+
+    this.rentals.push(rental);
+
+    return rental;
+  }
+
+  async findByCarId(car_id: string): Promise<Rental> {
+    const rental = await this.rentals.find(
+      (rental) => rental.car_id === car_id && !rental.end_date
+    );
+
+    return rental;
+  }
+
+  async findByUserId(user_id: string): Promise<Rental> {
+    const rental = await this.rentals.find(
+      (rental) => rental.user_id === user_id && !rental.end_date
+    );
+
+    return rental;
+  }
+}
+
+export { RentalRepositoryInMemory };
